d114f8053e67477b40ff70e0788a5944ca14cbb9
Pankaj <pankaj200635@gmail.com>
2021-09-28 18:10:18 +0530

    Search News API Hit

--- app/src/main/java/com/example/mynewsappversion/utils/Constants.kt
+++ app/src/main/java/com/example/mynewsappversion/utils/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "aa62a71b67de441c9c7c48ddd35a5fe7"
         const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
